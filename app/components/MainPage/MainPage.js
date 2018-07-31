02232e77faabae036e29822d1b385b45f2ed5e63
Krish Patel <kkpatel@bu.edu>
2018-07-30 22:09:55 -0700

    Page Passed

--- app/components/MainPage/MainPage.js
+++ app/components/MainPage/MainPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Button, Checkbox, Form, Divider} from 'semantic-ui-react'
+import { Header, Button, Checkbox, Form, Divider} from 'semantic-ui-react'
 import Login from '../Login/Login.js';
 import SignUp from '../SignUp/SignUp.js';
 import './MainPage.css';
@@ -8,19 +8,23 @@ import './MainPage.css';
 
 let MainPage = ({}) => {
   return (
-    <div>
-      <h1 className="Title"> Welcome to Metis, Event Planning </h1>
-      <div className="intro-page">
-        <div className="Login">
-          <Login />
+    <div className="entire-page">
+      <h1 className="header"> Welcome to Metis </h1>
+
+        <div className="main-page">
+          <div className="Login">
+            <Header className="loginHeader" size="large"> Log In </Header>
+            <Login />
+          </div>
+
+          <Divider vertical> Or </Divider>
+
+          <div className="SignUp">
+            <Header className="signupHeader" size="large"> Sign Up </Header>
+            <SignUp />
+          </div>
         </div>
 
-        <Divider vertical> Or </Divider>
-
-        <div className="SignUp">
-          <SignUp />
-        </div>
-      </div>
     </div>
   )
 }
