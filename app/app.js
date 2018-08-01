1ab45b191b0383e39a55339b1ef71d7ab2b3d3a5
Krish Patel <kkpatel@bu.edu>
2018-07-31 20:48:21 -0700

    CSS Done

--- app/app.js
+++ app/app.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MainPage from './components/MainPage/MainPage.js'
-
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-
   <MainPage />,
-
   document.getElementById('root')
 )
+
+registerServiceWorker();
