d15c287f978d62e6b395511721e0b2f41a63fd8a
perrylya <perrylya@gmail.com>
2018-08-01 21:31:42 -0700

    calendar

--- app/components/UserDash/UserDash.js
+++ app/components/UserDash/UserDash.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
-import { Menu, Input} from 'semantic-ui-react'
+import { Menu, Input, Table, Icon, Label} from 'semantic-ui-react'
 import './UserDash.css';
+import CalendarView from './CalendarView'
 
 class UserDash extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
     };
   }
 
 
   render() {
     return (
-      <div className="dashboard">
-        <span className="greetings"> Good Evening ~Add Name Here~ </span>
+      <div>
+        <div className="dashboard">
+          <span className="greetings"> Good Evening ~Add Name Here~ </span>
+        </div>
+        <CalendarView />
       </div>
     )
   }
 };
 
-
-export default UserDash;
+export default UserDash
