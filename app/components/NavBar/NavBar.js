6ce02e6ed8c524e539c13195f1d3c934c9c41496
Krish Patel <kkpatel@bu.edu>
2018-08-13 09:22:19 -0700

    Can go back to homepage now

--- app/components/NavBar/NavBar.js
+++ app/components/NavBar/NavBar.js
@@ -12,15 +12,15 @@ class UserDash extends React.Component {
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name})
 
-  handleLogout = () => {
-    
+  goHome = () => {
+    this.props.socket.emit('goHome')
   }
 
   render() {
     return (
       <div className="navBar">
         <Menu>
-          <Menu.Item name='logo here' active={this.state.activeItem === 'logoHere'} onClick={this.handleItemClick} />
+          <Menu.Item name='go home' active={this.state.activeItem === 'goHome'} onClick={this.goHome} />
           <Menu.Item name='about us' active={this.state.activeItem === 'aboutUs'} onClick={this.handleItemClick} />
           <Menu.Menu position='right'>
             <Menu.Item>
